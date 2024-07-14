3e7232f386557afed234ee17e204678adb93134a
Ankit-015 <ankitlogin15@gmail.com>
2024-07-14 13:46:35 +0530

    added

--- Node-Lecture/app.js
+++ Node-Lecture/app.js
@@ -1,4 +1,9 @@
-console.log("Start");
+const http = require('http');
 
+const routes = require('./routes');
 
-console.log("Check")
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
+
+server.listen(3000);
